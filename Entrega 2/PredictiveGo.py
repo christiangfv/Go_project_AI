498724b2baea87ca8d29e870f9f945451847f11b
M.ArayaF <mario.arayaf@sansano.usm.cl>
2020-12-03 02:49:25 -0300

    Todo en Numpy, IA_lvl1 y IA_lvl2, funcionando correctamente, filtro de jugadas en cada nivel

--- Entrega 2/PredictiveGo.py
+++ Entrega 2/PredictiveGo.py
@@ -7,84 +7,105 @@ from gym_go import govars, gogame
 from copy import copy  ## Copia profunda de un objeto, no referencia al mismo!!
 
 # Obtiene los movimientos invalidos desde el dict de info y las jugadas previas
-def get_invalidMoves(played, invalid_moves):
-    not_valid_moves = copy(played)
+def get_invalidMoves(invalid_moves):
+    not_valid_moves = np.empty([0,0])
+    
     for i in range(len(invalid_moves)):
         if invalid_moves[i] == 1:
-            not_valid_moves.append(i)
+            not_valid_moves = np.append(not_valid_moves, i)
+    
     return not_valid_moves
 
-def predict(jugadas, level):
+def predict(go_env, info_env, level, player):
     
-    go_env_pred = gym.make('gym_go:go-v0', size=args.boardsize, komi=args.komi)
-    #print(jugadas)
-
-    info = {"invalid_moves": np.zeros([49,1])}
-
-    for i in jugadas:
-        state, reward, done, info = go_env_pred.step(i) # Actualiza nuevo entorno
+    go_env_pred = copy(go_env)
+    info = info_env 
 
     nextPlays = []
-    invalid_moves = get_invalidMoves(jugadas, info["invalid_moves"])
+    invalid_moves = get_invalidMoves(info["invalid_moves"])
     #playsinthefuture = np.count_nonzero(info["invalid_moves"] == 0) - 1
 
-    nextPlays = seeInFurture(go_env_pred, invalid_moves, level)
-    print(nextPlays)
-    black_area, white_area = gogame.areas(go_env_pred.state_)
-    if nextPlays != [] and nextPlays[0][1] >= white_area:
-        play = random.randrange(len(nextPlays)) # Si hay más de una jugada que promete un mismo max ptj, se elige al azar
-        return nextPlays[play][0]
-    else:
-        return 49 # Esta jugada es equivalente a pasar, PASS!!!
+    nextPlays = seeInFurture(go_env_pred, invalid_moves, level, player)
+    print(nextPlays.flatten())
     
-    #black_area, white_area = gogame.areas(go_env_pred.state_)
-    #print(black_area, white_area)
-    #print('Con la jugada '+str(nextPlays[0])+' se consiguen '+str(black_area)+' puntos en '+str(playsinthefuture)+' jugadas')
+    black_area, white_area = gogame.areas(go_env_pred.state_)
+    
+    if player == "white":
+        if nextPlays.size != 0 and nextPlays[0, 1] > 0: # or nextPlays[0, 2] < black_area:
+            
+            play = random.randrange(len(nextPlays)) # Si hay más de una jugada que promete un mismo max ptj, se elige al azar
+            return int(nextPlays[play, 0])
+        else:
+            return 49
+    else:
+        if nextPlays.size != 0 and nextPlays[0, 1] > 0: # or nextPlays[0, 2] < black_area:
+            
+            play = random.randrange(len(nextPlays)) # Si hay más de una jugada que promete un mismo max ptj, se elige al azar
+            return int(nextPlays[play, 0])
+        else:
+            return 49
+        
 
-
-def seeInFurture(go_env_pred, invalidPlays, lvls, first = True):
+def seeInFurture(go_env_pred, invalidPlays, lvls, player, first = True):
     counter = 0
-    playPoints = []
+    playPoints = np.empty([0,2])
     maxPoints = 0
-    parentMove = []
+    tmpPoints = 0
+    parentMove = np.empty([0,0])
+
     for counter in range(49):
         if counter not in invalidPlays:
-            if lvls == 1: ## Captura el puntaje de las jugadas de nivel n
-                tmp_env = copy(go_env_pred)
-                tmp_env.step(counter)
-                black_area, white_area = gogame.areas(tmp_env.state_)
-                playPoints.append([counter, white_area]) # Si solo se visualiza 1 partida, esto guardara los ptjs 
-                if white_area > maxPoints:
-                    maxPoints = white_area
-            else:
-                parentMove.append(counter) # Si no estamos en el nivel n, guarda todas las jugadas posibles del nivel actual
+            # Captura el puntaje de las jugadas de nivel n
+            tmp_env = copy(go_env_pred)
+            prev_black_area, prev_white_area = gogame.areas(tmp_env.state_)
+            tmp_env.step(counter)
+            black_area, white_area = gogame.areas(tmp_env.state_)
 
+            # Guarda mejores ptjs, si no es lvl == 1, crea una lista de movimientos prometedores.
+            if player == "white":
+                white_pts = white_area - prev_white_area # area ganada
+                black_pts = prev_black_area - black_area # area quitada
+
+            else:
+                white_pts = prev_white_area - white_area # area ganada
+                black_pts = black_area - prev_black_area # area quitada
+
+            pts = white_pts + black_pts # area ganada + area quitada
+
+            if lvls == 1: # Crea lista de jugadas prometedoras del nivel más profundo y setea el ptj maximo
+                if pts > maxPoints:
+                    maxPoints = pts  
+                    playPoints = np.array([[counter, pts]])
+                elif pts == maxPoints:
+                    playPoints = np.append(playPoints, [[counter, pts]], axis=0)
+            else: # Si no es el lvl 1, crea lista con jugadas prometedoras a analizar
+                if pts > tmpPoints:
+                    tmpPoints = pts
+                    parentMove = np.array([counter])
+                elif pts == tmpPoints:
+                    parentMove = np.append(parentMove, counter)
+            
     lvls = lvls - 1 # Bajamos un nivel en el arbol
 
     if lvls: # Si llegamos al nivel 0, significa que ya pasamos el ultimo nivel, es decir el 1
         tmp_max = 0
+
         for i in parentMove: # Llama recursivamente a seeInFuture y obtiene el max ptj de esa rama
             tmp_env = copy(go_env_pred)
-            state, reward, done, info = tmp_env.step(i)
-            tmp_plays = copy(invalidPlays)
-            tmp_plays.append(i)
-            tmp_plays = get_invalidMoves(tmp_plays, info["invalid_moves"])
-            black_area, white_area = gogame.areas(tmp_env.state_)
-            if white_area > maxPoints:
-                tmp_max = white_area
-            tmp_max = seeInFurture(tmp_env, tmp_plays, lvls, False)
+            state, reward, done, info = tmp_env.step(int(i)) #juega el blanco
+            state, reward, done, info = tmp_env.step(49) # negras pasan
+            tmp_plays = get_invalidMoves(info["invalid_moves"])
+            tmp_max = seeInFurture(tmp_env, tmp_plays, lvls, player, False)
+
             if tmp_max > maxPoints and not first:
                 maxPoints = tmp_max
+
             elif first: # si estamos en la rama principal, es decir, siguiente jugada, guardamos jugada y max pje de la rama
                 if tmp_max == maxPoints: # Si tienen igual ptj se añade a la lista
-                    playPoints.append([i, tmp_max])
+                    playPoints = np.append(playPoints, [[i, tmp_max]], axis=0)
                 elif tmp_max > maxPoints: # Si se encuentr un ptj mayor, se resetea la lista y setea el max
-                    playPoints = [[i, tmp_max]]
+                    playPoints = np.array([[i, tmp_max]])
                     maxPoints = tmp_max
-
-    #if maxPoints == 0: # Si no hay suficientes jugadas al futuro se queda con el ptj actual de este nivel
-    #    black_area, white_area = gogame.areas(go_env_pred.state_)
-    #    maxPoints = white_area
 
     if first: # Si es el primer nivel, devolvemos la jugada junto al pje max de sus hijos
         maxPoints = playPoints
@@ -93,10 +114,10 @@ def seeInFurture(go_env_pred, invalidPlays, lvls, first = True):
 
 def valid_action(action, invalid_moves):
     if action is None:
-        return True
-    
+        return True  
+
     n = action[0]*7 + action[1]
-    #print(n, invalid_moves[n])
+    
     return not invalid_moves[n]
 
 if __name__ == "__main__":
@@ -118,27 +139,39 @@ if __name__ == "__main__":
         #           SETUP 
         #--------------------------------------------- 
         play = False      # if the user wants to play
-        n = input("You want to do: \n[1] IA vs IA \n[2] Human vs IA \n")
-        if n == '1':
-            play = False
-        elif n == '2':
-            play = True
-        else:
-            print("{ ERROR: invalid input >:C }")
-            exit(1)
-
-
-        #List of actions
-        actions = []
+        while not play:
+            n = input("What do You want to do?: \n[1] IA vs IA \n[2] Human vs IA \n[3] Exit\nSelect option: ")
+            if n == '1':
+                play = 1
+            elif n == '2':
+                play = 2
+            elif n == "3":
+                play = 3
+                print("\nSee ya later!, please come back :D and be destroyed :3\n")
+                exit(1)
+            else:
+                print("\n{ ERROR: invalid input >:C }\n")
+                continue
 
         # Initialize environment
         go_env = gym.make('gym_go:go-v0', size=args.boardsize, komi=args.komi)
 
-        if(play):
+        info = {"invalid_moves": np.zeros([49,1])}
+
+        if(play == 2):
             print("\n-----------------------")
             print("     Human vs IA")
             print("-----------------------\n")
-            
+            while True:
+                try:
+                    ia_lvl = int(input("Elija nivel de la maquina [1-2]: "))
+                    
+                    if ia_lvl < 3 and ia_lvl > 0:
+                        break 
+                    else:
+                        print("\nEsos niveles no estan disponibles por el momento :c\n")
+                except:
+                    print("\nIngrese un valor valido por favor\n")
             # First human action
             done = False
             invalid_moves = np.zeros([49,1])
@@ -163,43 +196,38 @@ if __name__ == "__main__":
                             continue
 
                 state, reward, done, info = go_env.step(action)
-                actions.append(action)
                 print("Black: ", action)
 
                 if go_env.game_ended():
                     break
 
                 # IA turn (w)
-                action = predict(actions,2)
-                actions.append(action)
+                action = predict(go_env, info, ia_lvl, "white")
                 state, reward, done, info = go_env.step(action)
                 print("White: ", action)
                 invalid_moves = info['invalid_moves']
 
-                if go_env.game_ended():
-                    break
+            go_env.render("human")
+            input("Presione cualquier botón para continuar...")
 
 
 
-        else: 
+        elif(play == 1): 
             print("\n-----------------------") 
             print("     IA vs IA") 
             print("-----------------------\n") 
-            action = predict(actions,1)#go_env.uniform_random_action()
+            action = predict(go_env, info, 1, "black")#go_env.uniform_random_action()
             print("Black: "+str(action))
 
-            actions.append(action)
             state, reward, done, info = go_env.step(action)
-            #go_env.render(mode="terminal")
 
             while not done:   # el ciclo termina cuando acaba el juego!!
                             # mientras tanto se dedicará a guardar las jugadas actuales en actions
 
                 #White turn
-                action = predict(actions,2)
+                action = predict(go_env, info, 2, "white")
 
                 print("White: "+str(action))
-                actions.append(action)
                 state, reward, done, info = go_env.step(action)
                 #End White turn
 
@@ -209,17 +237,14 @@ if __name__ == "__main__":
                     break
 
                 # Black turn
-                action = predict(actions,1)
+                action = predict(go_env, info, 1, "black")
                 print("Black: "+str(action))
-                actions.append(action)
                 state, reward, done, info = go_env.step(action)
                 #End Black turn
                 go_env.render(mode="terminal")
 
-            # go_env.render("terminal")
             go_env.render("human")
             input("Presione cualquier botón para continuar...")
-
 
 
 
